fd8712826442ddcc650189d4b53dada1caa8c1b1
gggyy3 <1587644910@qq.com>
2022-04-12 19:55:29 +0800

    tinymce插件宽度自适应

--- src/components/TEditor.vue
+++ src/components/TEditor.vue
@@ -1,153 +1,148 @@
 <template>
-    <div class="activeConfig">
-        <div class="activeConfig-container">
-            <Editor id="tinymce" v-model="tinymceHtml" :init="editorInit" />
-        </div>
+  <div class="activeConfig">
+    <div class="activeConfig-container">
+      <Editor id="tinymce" v-model="tinymceHtml" :init="editorInit" />
     </div>
+  </div>
 </template>
 
 <script>
-    // 引入组件
-    import tinymce from 'tinymce/tinymce'
-    import Editor from '@tinymce/tinymce-vue'
-    // 引入富文本编辑器主题的js和css
-    import 'tinymce/themes/silver/theme.min.js'
-    import 'tinymce/skins/ui/oxide/skin.min.css'
-    // 扩展插件
-    import 'tinymce/plugins/image'
-    import 'tinymce/plugins/media'
-    import 'tinymce/plugins/link'
-    import 'tinymce/plugins/code'
-    import 'tinymce/plugins/table'
-    import 'tinymce/plugins/lists'
-    import 'tinymce/plugins/paste'
-    import 'tinymce/plugins/imagetools'
-    import 'tinymce/plugins/print'
-    import 'tinymce/plugins/preview'
-    import 'tinymce/plugins/searchreplace'
-    import 'tinymce/plugins/autolink'
-    import 'tinymce/plugins/directionality'
-    import 'tinymce/plugins/visualblocks'
-    import 'tinymce/plugins/visualchars'
-    import 'tinymce/plugins/fullscreen'
-    import 'tinymce/plugins/template'
-    import 'tinymce/plugins/codesample'
-    import 'tinymce/plugins/charmap'
-    import 'tinymce/plugins/hr'
-    import 'tinymce/plugins/pagebreak'
-    import 'tinymce/plugins/nonbreaking'
-    import 'tinymce/plugins/anchor'
-    import 'tinymce/plugins/insertdatetime'
-    import 'tinymce/plugins/advlist'
-    import 'tinymce/plugins/wordcount'
-    import 'tinymce/plugins/textpattern'
-    import 'tinymce/plugins/help'
-    import 'tinymce/plugins/emoticons'
-    import 'tinymce/plugins/autosave'
+// 引入组件
+import tinymce from "tinymce/tinymce";
+import Editor from "@tinymce/tinymce-vue";
+// 引入富文本编辑器主题的js和css
+import "tinymce/themes/silver/theme.min.js";
+import "tinymce/skins/ui/oxide/skin.min.css";
+// 扩展插件
+import "tinymce/plugins/image";
+import "tinymce/plugins/media";
+import "tinymce/plugins/link";
+import "tinymce/plugins/code";
+import "tinymce/plugins/table";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/paste";
+import "tinymce/plugins/imagetools";
+import "tinymce/plugins/print";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/searchreplace";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/directionality";
+import "tinymce/plugins/visualblocks";
+import "tinymce/plugins/visualchars";
+import "tinymce/plugins/fullscreen";
+import "tinymce/plugins/template";
+import "tinymce/plugins/codesample";
+import "tinymce/plugins/charmap";
+import "tinymce/plugins/hr";
+import "tinymce/plugins/pagebreak";
+import "tinymce/plugins/nonbreaking";
+import "tinymce/plugins/anchor";
+import "tinymce/plugins/insertdatetime";
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/wordcount";
+import "tinymce/plugins/textpattern";
+import "tinymce/plugins/help";
+import "tinymce/plugins/emoticons";
+import "tinymce/plugins/autosave";
+import "tinymce/plugins/autoresize";
 
-    import 'tinymce/plugins/autoresize'
-    //import 'tinymce/plugins/formatpainter'
+import "tinymce/plugins/autoresize";
+//import 'tinymce/plugins/formatpainter'
 
-    export default {
-        name: 'ActiveConfig',
-        components: { Editor },
-        data() {
-            return {
-                // tinymce的绑定值
-                tinymceHtml: '',
-                // tinymce的初始化配置
-                editorInit: {
-                    //tinumce容器
-                    selector: '#tinymce',
-                    //配置语言
-                    language_url: '/tinymce/langs/zh_CN.js',
-                    language: 'zh_CN',
-                    //配置皮肤
-                    skin_url: '/tinymce/skins/ui/oxide',
-                    //配置编辑器高度
-                    height:500,
-                    min_height: 300,
-                    max_height:650,
-                    width:1000,
-                    min_width:900,
-                    max_width:1200,
-                    emoticons_database_url: '/emojis.js',
-                    emoticons_append: {
-                        "diy1": {
-                            "keywords": ["diy1"],
-                            "char": "\uD83E\uDD2F",
-                            "category": "animals_and_nature"
-                        }
-                    },
-                    // 此处为图片上传处理函数
-                    iimages_upload_base_path: '/demo',
-                    images_upload_handler: function (blobInfo, succFun, failFun) {
-                        var xhr, formData;
-                        var file = blobInfo.blob();//转化为易于理解的file对象
-                        xhr = new XMLHttpRequest();
-                        xhr.withCredentials = false;
-                        xhr.open('POST', '/demo/upimg2.php');
-                        xhr.onload = function() {
-                            var json;
-                            if (xhr.status != 200) {
-                                failFun('HTTP Error: ' + xhr.status);
-                                return;
-                            }
-                            json = JSON.parse(xhr.responseText);
-                            if (!json || typeof json.location != 'string') {
-                                failFun('Invalid JSON: ' + xhr.responseText);
-                                return;
-                            }
-                            succFun(json.location);
-                        };
-                        formData = new FormData();
-                        formData.append('file', file, file.name );
-                        xhr.send(formData);
-                    },
-                    fontsize_formats: '12px 14px 16px 18px 24px 36px 48px 56px 72px',
-                    font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;',
-                    link_list: [
-                        { title: '预置链接1', value: 'http://www.tinymce.com' },
-                        { title: '预置链接2', value: 'http://tinymce.ax-z.cn' }
-                    ],
-                    image_list: [
-                        { title: '预置图片1', value: 'https://www.tiny.cloud/images/glyph-tinymce@2x.png' },
-                        { title: '预置图片2', value: 'https://www.baidu.com/img/bd_logo1.png' }
-                    ],
-                    image_class_list: [
-                        { title: 'None', value: '' },
-                        { title: 'Some class', value: 'class-name' }
-                    ],
-                    importcss_append: true,
-                    //自定义文件选择器的回调内容
-                    file_picker_callback: function (callback, value, meta) {
-                        if (meta.filetype === 'file') {
-                            callback('https://www.baidu.com/img/bd_logo1.png', { text: 'My text' });
-                        }
-                        if (meta.filetype === 'image') {
-                            callback('https://www.baidu.com/img/bd_logo1.png', { alt: 'My alt text' });
-                        }
-                        if (meta.filetype === 'media') {
-                            callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.baidu.com/img/bd_logo1.png' });
-                        }
-                    },
-                    autosave_ask_before_unload: false,
-                    // 水印“Powered by TinyMCE”
-                    branding: false,
-                    statusbar: false,
-                }
-            }
+export default {
+  name: "ActiveConfig",
+  components: { Editor },
+  data() {
+    return {
+      // tinymce的绑定值
+      tinymceHtml: "",
+      // tinymce的初始化配置
+      editorInit: {
+        //tinumce容器
+        selector: "#tinymce",
+        //配置语言
+        language_url: "/tinymce/langs/zh_CN.js",
+        language: "zh_CN",
+        //配置皮肤
+        skin_url: "/tinymce/skins/ui/oxide",
+        //配置编辑器高度
+        height: 500,
+        min_height: 300,
+        max_height: 650,
+        // width:1000,
+        // min_width:900,
+        // max_width:1200,
+        emoticons_database_url: "/emojis.js",
+        emoticons_append: {
+          diy1: {
+            keywords: ["diy1"],
+            char: "\uD83E\uDD2F",
+            category: "animals_and_nature",
+          },
         },
-        mounted() {
-            tinymce.init({})
+        fontsize_formats: "12px 14px 16px 18px 24px 36px 48px 56px 72px",
+        font_formats:
+          "微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC,sans-serif;苹果苹方=PingFang SC,Microsoft YaHei,sans-serif;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei,sans-serif;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;",
+        link_list: [
+          { title: "预置链接1", value: "http://www.tinymce.com" },
+          { title: "预置链接2", value: "http://tinymce.ax-z.cn" },
+        ],
+        image_list: [
+          {
+            title: "预置图片1",
+            value: "https://www.tiny.cloud/images/glyph-tinymce@2x.png",
+          },
+          {
+            title: "预置图片2",
+            value: "https://www.baidu.com/img/bd_logo1.png",
+          },
+        ],
+        image_class_list: [
+          { title: "None", value: "" },
+          { title: "Some class", value: "class-name" },
+        ],
+        importcss_append: true,
+        //自定义文件选择器的回调内容
+        file_picker_callback: function (callback, value, meta) {
+          if (meta.filetype === "file") {
+            callback("https://www.baidu.com/img/bd_logo1.png", {
+              text: "My text",
+            });
+          }
+          if (meta.filetype === "image") {
+            callback("https://www.baidu.com/img/bd_logo1.png", {
+              alt: "My alt text",
+            });
+          }
+          if (meta.filetype === "media") {
+            callback("movie.mp4", {
+              source2: "alt.ogg",
+              poster: "https://www.baidu.com/img/bd_logo1.png",
+            });
+          }
         },
-        methods: {
-        }
-    }
+        autosave_ask_before_unload: false,
+        // 水印“Powered by TinyMCE”
+        branding: false,
+        statusbar: false,
+      },
+    };
+  },
+  mounted() {
+    tinymce.init({
+      plugins: "autoresize",
+      width: "100%",
+      height: 400,
+      autoresize_min_height: 400,
+      autoresize_max_height: 800,
+    });
+  },
+  methods: {},
+};
 </script>
 
 <style scoped>
-.activeConfig {
+.activeConfig-container {
   width: 100%;
 }
 </style>
